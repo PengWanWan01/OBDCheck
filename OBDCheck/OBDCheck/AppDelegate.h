64be87baffbdbcd73fe95f073b7fe5df7ffae433
yutaozhao <yutaozhao@macbook-pro.local>
2017-07-31 11:01:07 +0800

    创建工程

---
+++ OBDCheck/OBDCheck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OBDCheck
+//
+//  Created by yutaozhao on 2017/7/31.
+//  Copyright © 2017年 Auptophix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OBDCheck/OBDCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OBDCheck
+//
+//  Created by yutaozhao on 2017/7/31.
+//  Copyright © 2017年 Auptophix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
