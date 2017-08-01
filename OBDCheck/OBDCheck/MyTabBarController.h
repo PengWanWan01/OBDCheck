ac1a2d6cdc98544f4a048f0bb0557eb08ac3988c
yutaozhao <yutaozhao@macbook-pro.local>
2017-08-01 13:50:23 +0800

    创建Tabbar

---
+++ OBDCheck/OBDCheck/MyTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MyTabBarController.h
+//  OBDCheck
+//
+//  Created by yutaozhao on 2017/8/1.
+//  Copyright © 2017年 Auptophix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTabBarController : UITabBarController
+
+@end
