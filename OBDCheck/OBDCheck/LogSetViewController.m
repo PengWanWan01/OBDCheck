617a85ea3d07a278678dd246ce649b69999ea4b2
yutaozhao <yutaozhao@macbook-pro.local>
2017-09-12 17:40:13 +0800

    添加细节

--- OBDCheck/OBDCheck/LogSetViewController.m
+++ OBDCheck/OBDCheck/LogSetViewController.m
@@ -11,6 +11,7 @@
 @interface LogSetViewController ()<UITableViewDelegate,UITableViewDataSource,selectSwtichDelegete>
 {
     UITableView *MYtableView;
+    LogsModel *model;
 }
 @property(nonatomic,strong)NSMutableArray *titleLabel;
 @property(nonatomic,strong)NSMutableArray *selectLabel;
@@ -25,11 +26,19 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSArray* pAll = [LogsModel bg_findAll];
+    for(LogsModel* logsmodel in pAll){
+        NSLog(@"logsmodel.item1PID %@",logsmodel.item1PID  );
+        model = logsmodel;
+    }
+    [self initWithUI];
+}
+- (void)initWithUI{
     MYtableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, MSWidth, MSHeight-74) style:UITableViewStyleGrouped];
     MYtableView.backgroundColor = [UIColor clearColor];
     MYtableView.dataSource = self;
     MYtableView.delegate = self;
-   MYtableView.separatorColor = [ColorTools colorWithHexString:@"#212329"];
+    MYtableView.separatorColor = [ColorTools colorWithHexString:@"#212329"];
     [self.view addSubview:MYtableView];
     self.dataSource = [[NSMutableArray alloc]initWithObjects:@"Select",@"Enabled",@"Smoothing", nil];
     self.titleLabel = [[NSMutableArray alloc]initWithObjects:@"Graph Item 1",@"Graph Item 2",@"Graph Item 3",@"Graph Item 4", nil];
@@ -38,7 +47,6 @@
     [MYtableView registerNib:[UINib nibWithNibName:@"selectTableViewCell" bundle:nil] forCellReuseIdentifier:@"selectTableViewCell"];
     [MYtableView registerNib:[UINib nibWithNibName:@"StyleThreeTableViewCell" bundle:nil] forCellReuseIdentifier:@"StyleThreeTableViewCell"];
 }
-
 #pragma mark UITableViewDelegate,UITableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 4;
@@ -109,24 +117,118 @@
     }else{
         if (indexPath.section == 0 ||indexPath.section == 1 ) {
             StylethreeCell.titleName.text = self.dataSource[indexPath.row +1];
+            switch (indexPath.section) {
+                case 0:
+                {
+                    StylethreeCell.SwitchBtn.on = model.item1Smoothing;
+                }
+                    break;
+                case 1:
+                {
+                    StylethreeCell.SwitchBtn.on = model.item2Smoothing;
+                }
+                    break;
+                default:
+                    break;
+            }
         }
         else{
+            
             StylethreeCell.titleName.text = self.dataSource[indexPath.row];
+            switch (indexPath.section) {
+                case 2:
+                {
+                    switch (indexPath.row) {
+                        case 1:
+                        {
+                            StylethreeCell.SwitchBtn.on = model.item3Enabled;
+                        }
+                            break;
+                        case 2:
+                        {
+                            StylethreeCell.SwitchBtn.on = model.item3Smoothing;
+                        }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                    break;
+                case 3:
+                {
+                    switch (indexPath.row) {
+                        case 1:
+                        {
+                            StylethreeCell.SwitchBtn.on = model.item4Enabled;
+                        }
+                            break;
+                        case 2:
+                        {
+                            StylethreeCell.SwitchBtn.on = model.item4Smoothing;
+                        }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                    break;
+                default:
+                    break;
+            }
         }
          StylethreeCell.backgroundColor  = [ColorTools colorWithHexString:@"3B3F49"];
          StylethreeCell.selectionStyle = UITableViewCellSelectionStyleNone;
         StylethreeCell.delegate = self;
         StylethreeCell.SwitchBtn.tag = [[NSString stringWithFormat:@"%ld%ld",(long)indexPath.section,(long)indexPath.row] integerValue];
-        StylethreeCell.SwitchBtn.on = NO;
         resultCell = StylethreeCell;
     }
     return resultCell;
 }
 - (void)selectSwtichBetouched:(UISwitch  *)switchBtn{
     NSLog(@"tagtag%ld",(long)switchBtn.tag);
-    
+    switch (switchBtn.tag) {
+        case 1:
+        {
+            bg_setDebug(YES);
+            NSString *sql = [NSString stringWithFormat:@"SET item1Smoothing ='%d'",switchBtn.on];
+            [LogsModel bg_updateSet:sql];
+        }
+            break;
+        case 11:{
+            NSString *sql = [NSString stringWithFormat:@"SET item2Smoothing ='%d'",switchBtn.on];
+            [LogsModel bg_updateSet:sql];
+        }
+            break;
+        case 21:{
+            NSString *sql = [NSString stringWithFormat:@"SET item3Enabled ='%d'",switchBtn.on];
+            [LogsModel bg_updateSet:sql];
+        }
+            break;
+
+        case 22:{
+            NSString *sql = [NSString stringWithFormat:@"SET item3Smoothing ='%d'",switchBtn.on];
+            [LogsModel bg_updateSet:sql];
+        }
+            break;
+
+        case 31:{
+            NSString *sql = [NSString stringWithFormat:@"SET item4Enabled ='%d'",switchBtn.on];
+            [LogsModel bg_updateSet:sql];
+        }
+            break;
+        case 32:{
+            NSString *sql = [NSString stringWithFormat:@"SET item4Smoothing ='%d'",switchBtn.on];
+            [LogsModel bg_updateSet:sql];
+        }
+            break;
+        default:
+            break;
+    }
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-
+    if (indexPath.section == 0) {
+        PIDSelectViewController *VC = [[PIDSelectViewController alloc]init];
+        [self.navigationController pushViewController:VC animated:YES];
+    }
 }
 @end
--- OBDCheck/OBDCheck/LogsController.m
+++ OBDCheck/OBDCheck/LogsController.m
@@ -17,6 +17,10 @@ typedef NS_ENUM(NSInteger ,chartViewnumber)
 {
     LineChartView *chartViewone ;
     LineChartView *chartViewTwo ;
+    LogsModel *model;
+    LineChartDataSet *set1;
+    LineChartData *PartOnedata;
+    LineChartData *PartTwodata;
 
 }
 @end
@@ -25,48 +29,47 @@ typedef NS_ENUM(NSInteger ,chartViewnumber)
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [self initNavBarTitle:@"Logs" andLeftItemImageName:@"back" andRightItemImageName:@"other"];
+    set1 = nil;
+    PartOnedata = [[LineChartData alloc] initWithDataSet:set1];
+    PartTwodata = [[LineChartData alloc] initWithDataSet:set1];
+
     self.view.backgroundColor = [ColorTools colorWithHexString:@"#212329"];
+    NSArray* pAll = [LogsModel bg_findAll];
+    for(LogsModel* logsmodel in pAll){
+        NSLog(@"logsmodel.item1PID %@",logsmodel.item1PID  );
+        model = logsmodel;
+        if (model.item3Enabled == YES || model.item4Enabled == YES ) {
+            [self initWithLogViewTwoPart];
+        }else{
+            [self initWithLogView];
+        }
+    }
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self initWithLogView];
     [self initWithUI];
-    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(0, 20, 100, 20)];
-    btn.backgroundColor = [UIColor redColor];
-    [btn addTarget: self action:@selector(btn) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
-}
+  
+
+   }
 - (void)btn{
     NSLog(@"1212");
-    [self updateChartData];
+    [self updateChartData:chartViewone withData:PartOnedata withIndex:0 withX:100 withY:100];
 }
  - (void)initWithLogView{
+     [chartViewone removeFromSuperview];
+     [chartViewTwo removeFromSuperview];
     chartViewone = [[LineChartView alloc]initWithFrame:CGRectMake(0, 0, MSWidth, MSHeight - 45-64)];
     [self.view addSubview:chartViewone];
-    chartViewone.delegate = self;
+     UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(0, 20, 100, 20)];
+     btn.backgroundColor = [UIColor redColor];
+     [btn addTarget: self action:@selector(btn) forControlEvents:UIControlEventTouchUpInside];
+     [self.view addSubview:btn];
+
+    [self initWithchartType:chartViewone];
     
-    chartViewone.chartDescription.enabled = NO;
-    
-    chartViewone.dragEnabled = YES;
-    [chartViewone setScaleEnabled:YES];
-    chartViewone.pinchZoomEnabled = YES;
-    chartViewone.drawGridBackgroundEnabled = YES;
-    chartViewone.backgroundColor = [UIColor clearColor];
-    chartViewone.gridBackgroundColor = [UIColor clearColor];
-    //    chartView.highlightPerTapEnabled = NO;
-    
-    ChartLegend *l = chartViewone.legend;
-    l.form = ChartLegendFormLine;
-    l.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:11.f];
-    l.textColor = UIColor.whiteColor;
-    l.horizontalAlignment = ChartLegendHorizontalAlignmentLeft;
-    l.verticalAlignment = ChartLegendVerticalAlignmentBottom;
-    l.orientation = ChartLegendOrientationHorizontal;
-    l.drawInside = NO;
-    [self initWithchartType:1];
-    
-    [self setDataCount:100 range:110];
-    
+    [self setDataCount:100 range:110 withView:chartViewone withdata:PartOnedata withPIDTiltle:model.item1PID withLineColor:[ColorTools colorWithHexString:@"E51C23"] withDependency:AxisDependencyLeft iSsmoothing:(model.item1Smoothing)];
+     [self setDataCount:100 range:550 withView:chartViewone withdata:PartOnedata withPIDTiltle:model.item2PID withLineColor:[ColorTools colorWithHexString:@"54C44B"] withDependency:AxisDependencyRight iSsmoothing:(model.item2Smoothing)];
+     
     [chartViewone animateWithXAxisDuration:1];
      //设置当前可以看到的个数
      [chartViewone setVisibleXRangeMaximum:10];
@@ -74,101 +77,98 @@ typedef NS_ENUM(NSInteger ,chartViewnumber)
      [chartViewone moveViewToX:15];
      
 }
-- (void)updateChartData
+- (void)initWithLogViewTwoPart{
+    [chartViewone removeFromSuperview];
+    [chartViewTwo removeFromSuperview];
+    chartViewone = [[LineChartView alloc]initWithFrame:CGRectMake(0, 0, MSWidth, (MSHeight - 45-64)/2)];
+    chartViewTwo = [[LineChartView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(chartViewone.frame), MSWidth, (MSHeight - 45-64)/2)];
+    [self.view addSubview:chartViewone];
+     [self.view addSubview:chartViewTwo];
+    [self initWithchartType:chartViewone];
+    [self initWithchartType:chartViewTwo];
+    [self setDataCount:100 range:110 withView:chartViewone withdata:PartOnedata withPIDTiltle:model.item1PID withLineColor:[ColorTools colorWithHexString:@"E51C23"] withDependency:AxisDependencyLeft iSsmoothing:(model.item1Smoothing)];
+    [self setDataCount:100 range:550 withView:chartViewone withdata:PartOnedata withPIDTiltle:model.item2PID withLineColor:[ColorTools colorWithHexString:@"54C44B"] withDependency:AxisDependencyRight iSsmoothing:(model.item2Smoothing)];
+    [self setDataCount:100 range:110 withView:chartViewTwo withdata:PartTwodata withPIDTiltle:model.item3PID withLineColor:[ColorTools colorWithHexString:@"3F51B5"] withDependency:AxisDependencyLeft iSsmoothing:(model.item3Smoothing)];
+    if (model.item4Enabled == YES) {
+    [self setDataCount:100 range:550 withView:chartViewTwo withdata:PartTwodata withPIDTiltle:model.item4PID withLineColor:[ColorTools colorWithHexString:@"FF9800"] withDependency:AxisDependencyRight iSsmoothing:(model.item4Smoothing)];
+    }
+    [chartViewone animateWithXAxisDuration:1];
+    //设置当前可以看到的个数
+    [chartViewone setVisibleXRangeMaximum:10];
+    //设置当前开始的位置
+    [chartViewone moveViewToX:15];
+ 
+    [chartViewTwo animateWithXAxisDuration:1];
+    //设置当前可以看到的个数
+    [chartViewTwo setVisibleXRangeMaximum:10];
+    //设置当前开始的位置
+    [chartViewTwo moveViewToX:15];
+}
+//添加动态数据
+- (void)updateChartData:(LineChartView *)view withData:(LineChartData *)linechartdata withIndex:(NSInteger)index  withX:(int)X withY:(int)Y
 {
     
-    chartViewone.frame  = CGRectMake(0, 0, MSWidth +10, chartViewone.frame.size.height);
-    [self setDataCount:110 range:100];
-    [chartViewone setNeedsDisplay];
-    NSLog(@"widthwidth%f",chartViewone.frame.size.width);
     
+    [linechartdata addEntry:[[ChartDataEntry alloc]initWithX:X y:Y] dataSetIndex:index];
+      [linechartdata notifyDataChanged];
+        [view notifyDataSetChanged];
+    NSLog(@"updateChartData%ld",linechartdata.entryCount);
+
 }
 
-- (void)setDataCount:(int)count range:(double)range
+- (void)setDataCount:(int)count range:(double)range withView:(LineChartView *)view withdata:(LineChartData *)linechartdata withPIDTiltle:(NSString *)title withLineColor:(UIColor *)color withDependency:(AxisDependency)Dependency  iSsmoothing:(BOOL)smoothing
 {
-    NSMutableArray *yVals1 = [[NSMutableArray alloc] init];
-    NSMutableArray *yVals2 = [[NSMutableArray alloc] init];
-    NSMutableArray *yVals3 = [[NSMutableArray alloc] init];
+    NSMutableArray *yVals = [[NSMutableArray alloc] init];
     
     for (int i = 0; i < count; i++)
     {
         double mult = range / 2.0;
         double val = (double) (arc4random_uniform(mult)) + 50;
-        [yVals1 addObject:[[ChartDataEntry alloc] initWithX:i y:val]];
+        [yVals addObject:[[ChartDataEntry alloc] initWithX:i y:val]];
     }
+        set1 = [[LineChartDataSet alloc] initWithValues:yVals label:title];
+        set1.axisDependency = Dependency;
+    NSLog(@"colorcolor%@",color);
     
-    for (int i = 0; i < count - 1; i++)
-    {
-        double mult = range;
-        double val = (double) (arc4random_uniform(mult)) + 450;
-        [yVals2 addObject:[[ChartDataEntry alloc] initWithX:i y:val]];
-    }
-    
-    for (int i = 0; i < count; i++)
-    {
-        double mult = range;
-        double val = (double) (arc4random_uniform(mult)) + 500;
-        [yVals3 addObject:[[ChartDataEntry alloc] initWithX:i y:val]];
-    }
-    
-    LineChartDataSet *set1 = nil, *set2 = nil, *set3 = nil;
-    
-    if (chartViewone.data.dataSetCount > 0)
-    {
-        set1 = (LineChartDataSet *)chartViewone.data.dataSets[0];
-        set2 = (LineChartDataSet *)chartViewone.data.dataSets[1];
-        set3 = (LineChartDataSet *)chartViewone.data.dataSets[2];
-        set1.values = yVals1;
-        set2.values = yVals2;
-        set3.values = yVals3;
-        [chartViewone.data notifyDataChanged];
-        [chartViewone notifyDataSetChanged];
-    }
-    else
-    {
-        set1 = [[LineChartDataSet alloc] initWithValues:yVals1 label:@"DataSet 1"];
-        set1.axisDependency = AxisDependencyLeft;
-        set1.highlightColor = [UIColor colorWithRed:244/255.f green:117/255.f blue:117/255.f alpha:1.f];
+     [set1 setColor:color];  
+        set1.highlightColor = [UIColor clearColor]; //点击时候的颜色
         set1.drawCircleHoleEnabled = NO;
-        set1.lineWidth = 1.0/[UIScreen mainScreen].scale;//折线宽度
-        
-        set1.drawSteppedEnabled = NO;//是否开启绘制阶梯样式的折线图
+        set1.lineWidth = 2.0;//折线宽度
         //折线拐点样式
         set1.drawCirclesEnabled = NO;//是否绘制拐点
-        
-        set2 = [[LineChartDataSet alloc] initWithValues:yVals2 label:@"DataSet 2"];
-        set2.axisDependency = AxisDependencyRight;
-        [set2 setColor:UIColor.redColor];
-        set2.lineWidth = 1.0/[UIScreen mainScreen].scale;//折线宽度
-        
-        set2.drawSteppedEnabled = NO;//是否开启绘制阶梯样式的折线图
-        //折线拐点样式
-        set2.drawCirclesEnabled = NO;//是否绘制拐点
-        
-        set3 = [[LineChartDataSet alloc] initWithValues:yVals3 label:@"DataSet 3"];
-        set3.axisDependency = AxisDependencyRight;
-        [set3 setColor:UIColor.yellowColor];
-        set3.lineWidth = 1.0/[UIScreen mainScreen].scale;//折线宽度
-        set3.valueTextColor = [UIColor clearColor];
-        set3.drawSteppedEnabled = NO;//是否开启绘制阶梯样式的折线图
-        //折线拐点样式
-        set3.drawCirclesEnabled = NO;//是否绘制拐点
-        
-        NSMutableArray *dataSets = [[NSMutableArray alloc] init];
-        [dataSets addObject:set1];
-        [dataSets addObject:set2];
-        [dataSets addObject:set3];
-        
-        LineChartData *data = [[LineChartData alloc] initWithDataSets:dataSets];
-        [data setValueTextColor:UIColor.clearColor];
-        [data setValueFont:[UIFont systemFontOfSize:9.f]];
-        
-        chartViewone.data = data;
+    if (smoothing == YES) {
+         [set1 setDrawCubicEnabled:YES];
     }
+       [linechartdata addDataSet:set1];
+    
+        [linechartdata setValueTextColor:UIColor.clearColor];
+        [linechartdata setValueFont:[UIFont systemFontOfSize:9.f]];
+        
+        view.data = linechartdata;
+        
+    NSLog(@"%ld",view.data.entryCount);
+    
 }
 
-- (void)initWithchartType:(NSInteger )type{
-    ChartXAxis *xAxis = chartViewone.xAxis;
+- (void)initWithchartType:(LineChartView *)view{
+    view.delegate = self;
+    view.chartDescription.enabled = NO;
+    view.dragEnabled = YES;
+    [view setScaleEnabled:YES];
+    view.pinchZoomEnabled = YES;
+    view.drawGridBackgroundEnabled = YES;
+    view.backgroundColor = [UIColor clearColor];
+    view.gridBackgroundColor = [UIColor clearColor];
+    
+    ChartLegend *l = view.legend;
+    l.form = ChartLegendFormLine;
+    l.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:11.f];
+    l.textColor = UIColor.whiteColor;
+    l.horizontalAlignment = ChartLegendHorizontalAlignmentLeft;
+    l.verticalAlignment = ChartLegendVerticalAlignmentBottom;
+    l.orientation = ChartLegendOrientationHorizontal;
+    l.drawInside = NO;
+    ChartXAxis *xAxis = view.xAxis;
     xAxis.labelFont = [UIFont systemFontOfSize:11.f];
     xAxis.labelTextColor = UIColor.whiteColor;
     xAxis.drawGridLinesEnabled = YES;
@@ -176,7 +176,7 @@ typedef NS_ENUM(NSInteger ,chartViewnumber)
     xAxis.decimals = 6;
     xAxis.gridColor = [UIColor grayColor];
     
-    ChartYAxis *leftAxis = chartViewone.leftAxis;
+    ChartYAxis *leftAxis = view.leftAxis;
     leftAxis.labelTextColor = [UIColor whiteColor];
     leftAxis.axisMaximum = 200.0;
     leftAxis.axisMinimum = 0.0;
@@ -184,7 +184,7 @@ typedef NS_ENUM(NSInteger ,chartViewnumber)
     [leftAxis setAxisLineColor:[UIColor redColor]];
     leftAxis.axisLineWidth = 2;
     leftAxis.labelCount = 5;
-    ChartYAxis *rightAxis = chartViewone.rightAxis;
+    ChartYAxis *rightAxis = view.rightAxis;
     rightAxis.labelTextColor = [UIColor whiteColor];
     rightAxis.axisMaximum = 900.0;
     rightAxis.axisMinimum = -200.0;
