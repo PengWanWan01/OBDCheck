45dff397d9e4883531d53116dda014c510d2f9f7
yutaozhao <yutaozhao@macbook-pro.lan>
2017-11-03 17:40:03 +0800

    故障码保存

---
+++ OBDCheck/OBDCheck/historyCode.m
@@ -0,0 +1,13 @@
+//
+//  historyCode.m
+//  OBDCheck
+//
+//  Created by yutaozhao on 2017/11/3.
+//  Copyright © 2017年 Auptophix. All rights reserved.
+//
+
+#import "historyCode.h"
+
+@implementation historyCode
+
+@end
