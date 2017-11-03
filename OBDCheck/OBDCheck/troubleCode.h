ccba6a3adbcb88407bb7d695421b545c8ea7adce
yutaozhao <yutaozhao@macbook-pro.lan>
2017-11-03 15:58:59 +0800

    建立故障码数据库

---
+++ OBDCheck/OBDCheck/troubleCode.h
@@ -0,0 +1,13 @@
+//
+//  troubleCode.h
+//  OBDCheck
+//
+//  Created by yutaozhao on 2017/11/3.
+//  Copyright © 2017年 Auptophix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface troubleCode : NSObject
+@property (nonatomic,strong) NSArray *toubleCode;
+@end
---
+++ OBDCheck/OBDCheck/troubleCode.m
@@ -0,0 +1,13 @@
+//
+//  troubleCode.m
+//  OBDCheck
+//
+//  Created by yutaozhao on 2017/11/3.
+//  Copyright © 2017年 Auptophix. All rights reserved.
+//
+
+#import "troubleCode.h"
+
+@implementation troubleCode
+
+@end
