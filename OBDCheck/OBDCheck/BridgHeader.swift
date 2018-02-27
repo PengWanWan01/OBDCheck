e776d47a6b4a1412948999a21a1a702362a8be58
yutaozhao <yutaozhao@macbook-pro.local>
2018-02-27 15:55:44 +0800

    完善Chart第三方库的引入方式

--- OBDCheck/OBDCheck/BridgHeader.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BridgHeader.swift
-//  OBDCheck
-//
-//  Created by yutaozhao on 2017/9/11.
-//  Copyright © 2017年 Auptophix. All rights reserved.
-//
-
---
+++ OBDCheck/OBDCheck/OBDCheck_Swift.h
@@ -0,0 +1,14 @@
+//
+//  OBDCheck_Swift.h
+//  Charts
+//
+//  Created by yutaozhao on 2018/2/27.
+//
+
+#ifndef OBDCheck_Swift_h
+#define OBDCheck_Swift_h
+
+
+#endif /* OBDCheck_Swift_h */
+@import Charts;
+
